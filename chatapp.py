1a5e43a932936106bd67d08bbb67316539469ea0
Peristratus <testcorp24@gmail.com>
2021-01-21 05:05:15 +0000

    adding the code for routes, secret key, method = GET and Post

--- chatapp.py
+++ chatapp.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_socketio import SocketIO, join_room, leave_room, emit
+from flask_session import Session
 
-app=Flask(__name__)
+app =Flask(__name__)
+app.debug = True
+app.config['SECRET_TYPE'] = 'secret'
+app.config['SESSION_TYPE'] = "filesystem"
 
+Session(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "installation Successul"
+    return "render_template('index1.html')
+
+@app.route('/chat', methods=['GET', 'POST'])
+def chat():
+    if(request.method== 'POST'):
+        username = request.form['username']
+        room = request.form['room']
+        #store the data session
+        session['username'] = username
+        session['room'] = room
+        return render_template('chat.html', session = session)
+    else:
+        if(session.get('username') is not None):
+            return render_template('chat.html', session = session)
+        else:
+            return redirect(url_for('index'))
 
 if __name__ =="__main__":
     app.run()
